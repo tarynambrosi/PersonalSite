eeceea210e435dfc2eb1d285f7838b138709c085
tarynambrosi <tarynambrosi@gmail.com>
2024-03-02 19:25:23 -0600

    requirements met

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,3 +245,21 @@
   new PureCounter();
 
 })()
+
+//jQuery plugin
+$('a[href*="#"]').click(function(event) {
+  if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+  ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+  }
+}); 
